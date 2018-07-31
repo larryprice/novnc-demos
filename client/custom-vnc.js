e011be800996cb4573052869a082600566725d0f
Larry Price <lrp@illinoisrocstar.com>
2018-07-31 15:37:12 -0500

    Only prompt for password when we need it

--- client/custom-vnc.js
+++ client/custom-vnc.js
@@ -10,17 +10,31 @@ const disconnect = (e) => {
   }
 
   document.getElementById('connect-setup').classList.remove('hidden')
+  document.getElementById('connect-form').classList.remove('hidden')
+  document.getElementById('connect-password').classList.add('hidden')
   document.getElementById('connected-view').classList.add('hidden')
 
   rfb = null
 }
 
+const onConnected = () => {
+  console.debug(`Connected to ${rfb._url}`)
+  document.getElementById('connect-setup').classList.add('hidden')
+  document.getElementById('connect-password').classList.remove('hidden')
+  document.getElementById('connected-view').classList.remove('hidden')
+}
+
+const onPasswordPrompt = () => {
+  document.getElementById('connect-form').classList.add('hidden')
+  document.getElementById('connect-password').classList.remove('hidden')
+}
+
 const connect = (address, password) => {
   try {
-    rfb = new RFB(document.getElementById('noVNC-canvas'), `ws://${address}`,
-      {credentials: {password}})
-    rfb.addEventListener('connect', () => console.debug(`Connected to ${address}`))
+    rfb = new RFB(document.getElementById('noVNC-canvas'), `ws://${address}`)
+    rfb.addEventListener('connect', onConnected)
     rfb.addEventListener('disconnect', disconnect)
+    rfb.addEventListener('credentialsrequired', onPasswordPrompt)
     rfb.scaleViewport = rfb.resizeSession = true
   } catch (err) {
     console.error('Unable to create RFB client:', err)
@@ -29,11 +43,13 @@ const connect = (address, password) => {
 
 window.addEventListener('DOMContentLoaded', e => {
   document.getElementById('disconnect-btn').addEventListener('click', disconnect)
+  document.getElementById('connect-password').addEventListener('submit', e => {
+    e.preventDefault()
+    rfb.sendCredentials({password: e.target[0].value})
+  })
   document.getElementById('connect-form').addEventListener('submit', e => {
     e.preventDefault()
 
-    connect(e.target[0].value, e.target[1].value)
-    document.getElementById('connect-setup').classList.add('hidden')
-    document.getElementById('connected-view').classList.remove('hidden')
+    connect(e.target[0].value)
   })
 })
