958e410355a061b144b398c7db7fb4ca3f82b750
Larry Price <lrp@illinoisrocstar.com>
2018-07-31 13:54:04 -0500

    Form with password

--- client/custom-vnc.js
+++ client/custom-vnc.js
@@ -1,19 +1,24 @@
 const RFB = require('novnc-core').default
 let rfb = null
 
-const disconnect = () => {
+const disconnect = (e) => {
   if (!rfb) return
 
-  console.debug(`Disconnecting from ${rfb._url}`)
+  if (!e.detail || e.detail.clean) {
+    console.debug(`Disconnecting from ${rfb._url}`)
+    rfb.disconnect()
+  }
+
   document.getElementById('connect-setup').classList.remove('hidden')
   document.getElementById('connected-view').classList.add('hidden')
-  rfb.disconnect()
+
   rfb = null
 }
 
-const connect = (address) => {
+const connect = (address, password) => {
   try {
-    rfb = new RFB(document.getElementById('noVNC-canvas'), `ws://${address}`)
+    rfb = new RFB(document.getElementById('noVNC-canvas'), `ws://${address}`,
+      {credentials: {password}})
     rfb.addEventListener('connect', () => console.debug(`Connected to ${address}`))
     rfb.addEventListener('disconnect', disconnect)
     rfb.scaleViewport = rfb.resizeSession = true
@@ -26,7 +31,8 @@ window.addEventListener('DOMContentLoaded', e => {
   document.getElementById('disconnect-btn').addEventListener('click', disconnect)
   document.getElementById('connect-form').addEventListener('submit', e => {
     e.preventDefault()
-    connect(e.target[0].value)
+
+    connect(e.target[0].value, e.target[1].value)
     document.getElementById('connect-setup').classList.add('hidden')
     document.getElementById('connected-view').classList.remove('hidden')
   })
